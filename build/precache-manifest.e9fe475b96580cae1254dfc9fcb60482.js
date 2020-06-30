121fe69d444e77a1bb969b95aa0b0dfd2db2efd8
Marc Strong <marcstrong@yahoo.com>
2020-06-29 23:40:30 -0500

    add server.js, update package

---
+++ build/precache-manifest.e9fe475b96580cae1254dfc9fcb60482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed93b656d2de41dee5b583126cdf44f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841cee6120772bcc4c9c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bb42f11f64d341a8976",
+    "url": "/static/js/2.786cda50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.786cda50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841cee6120772bcc4c9c",
+    "url": "/static/js/main.cce12a09.chunk.js"
+  },
+  {
+    "revision": "76c0879cc130edce90a2",
+    "url": "/static/js/runtime-main.2cea7679.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
